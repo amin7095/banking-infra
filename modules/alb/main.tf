7d5ece8815dcf5b79b984c0f4158cb1a9bcf11eb
Amin Momin <66801652+amin7095@users.noreply.github.com>
2025-11-16 12:49:03 +0000

    new

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -41,5 +41,8 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
-  default_action { type = "forward" target_group_arn = aws_lb_target_group.app.arn }
+  default_action {
+  type             = "forward"
+  target_group_arn = aws_lb_target_group.app.arn
+}
 }
--- modules/app_asg/main.tf
+++ modules/app_asg/main.tf
@@ -20,7 +20,12 @@ data "aws_ami" "al2023" {
 resource "aws_security_group" "app" {
   name   = "${var.name}-app-sg"
   vpc_id = var.vpc_id
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 }
 
 resource "aws_launch_template" "lt" {
