2771934924e1b6698a8179386c0953af9cf492e4
Amin Momin <66801652+amin7095@users.noreply.github.com>
2025-11-16 14:05:41 +0000

    test1

--- envs/main/variables.tf
+++ envs/main/variables.tf
@@ -1,15 +1,25 @@
+variable "aws_region" {
+  type = string
+}
 
-variable "aws_region"          { type = string }
-variable "env_name"            { type = string }
+variable "env_name" {
+  type = string
+}
+
 variable "instance_type" {
   type    = string
   default = "t3.medium"
 }
-variable "app_repo"            { type = string }
+
+variable "app_repo" {
+  type = string
+}
+
 variable "payment_mode" {
   type    = string
   default = "wiremock"
 }
+
 variable "db_username" {
   type      = string
   sensitive = true
@@ -20,29 +30,31 @@ variable "db_password" {
   sensitive = true
 }
 
-variable "dynamodb_table_name" { 
-  type = string  
-  default = "test-data" }
+variable "dynamodb_table_name" {
+  type    = string
+  default = "test-data"
+}
 
-variable "ssh_public_key"      { type = string }
+variable "ssh_public_key" {
+  type = string
+}
 
 variable "datadog_api_key" {
   type      = string
   sensitive = true
 }
 
-variable "datadog_site"        { 
-  type = string  
-  default = "datadoghq.eu" }
+variable "datadog_site" {
+  type    = string
+  default = "datadoghq.eu"
+}
 
 variable "gremlin_team_id" {
   type      = string
   sensitive = true
 }
 
-
 variable "gremlin_secret" {
   type      = string
   sensitive = true
 }
-
