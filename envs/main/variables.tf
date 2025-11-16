7780c74bf34a04f1c64a600b99459515a747d26a
Amin Momin <66801652+amin7095@users.noreply.github.com>
2025-11-16 13:46:13 +0000

    run-7

--- envs/main/variables.tf
+++ envs/main/variables.tf
@@ -10,11 +10,34 @@ variable "payment_mode" {
   type    = string
   default = "wiremock"
 }
-variable "db_username"         { type = string  sensitive = true }
-variable "db_password"         { type = string  sensitive = true }
+variable "db_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
 variable "dynamodb_table_name" { type = string  default = "test-data" }
 variable "ssh_public_key"      { type = string }
-variable "datadog_api_key"     { type = string  sensitive = true }
+
+variable "datadog_api_key" {
+  type      = string
+  sensitive = true
+}
+
 variable "datadog_site"        { type = string  default = "datadoghq.eu" }
-variable "gremlin_team_id"     { type = string  sensitive = true }
-variable "gremlin_secret"      { type = string  sensitive = true }
+
+variable "gremlin_team_id" {
+  type      = string
+  sensitive = true
+}
+
+
+variable "gremlin_secret" {
+  type      = string
+  sensitive = true
+}
+
